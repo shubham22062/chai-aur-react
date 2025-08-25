85909a446de347e21be134910ecfb4c75fbc9336
Silver <rajat.saraswat.0409@gmail.com>
2025-08-25 23:07:41 +0530

    Update Login.jsx

--- Chai-aur-React/08miniContext/src/components/Login.jsx
+++ Chai-aur-React/08miniContext/src/components/Login.jsx
@@ -18,6 +18,7 @@ function Login() {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 placeholder="username"
+                // there is no backend
             />
             {" "}
             <input
